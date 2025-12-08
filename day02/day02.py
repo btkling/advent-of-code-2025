dac0c7e21892e8d1354be8626fb85f5d858e5635
Ben Kling <kling.ben@gmail.com>
2025-12-08 14:32:30 -0500

    [day02] solved part 2

--- day02/day02.py
+++ day02/day02.py
@@ -15,6 +15,37 @@ def is_invalid_id(id:str):
         else:
             return False
 
+def split_string(string, num_pieces):
+    # evenly split string into num_pieces
+    pieces = []
+
+    for i in range(0, len(string), num_pieces):
+        pieces.append(string[0 + i: num_pieces + i])
+
+    return pieces
+
+
+def is_invalid_id_v2(id):
+    # print(f"  -> checking {id}")
+    flag = False
+    id = str(id)
+    slice_len = 1  # start with a slice of length 1
+    id_len = len(id)
+
+    while slice_len <= int(id_len / 2):
+        # if slice length doesn't evenly divide across the whole string,
+        # it's not possible to only repeat on the slice
+        if id_len % slice_len == 0:
+            sliced_id = split_string(id, slice_len)
+            if len(set(sliced_id)) == 1:
+                flag = True
+            slice_len += 1
+        else:
+            slice_len += 1
+
+    return flag
+
+
 
 def get_invalid_ids(str_range:str):
     start = int(str_range.split("-")[0])
@@ -27,8 +58,7 @@ def get_invalid_ids(str_range:str):
 
     print(f"range from {start} to {end} has {length} elements")
     for id in rng_list:
-        # print(r)
-        if is_invalid_id(id):
+        if is_invalid_id_v2(id):
             invalid.append(id)
             print(f"  - found invalid id: {id}")
 
@@ -42,5 +72,4 @@ if __name__ == "__main__":
         for rng in line.split(","):
             new_invalid_ids = get_invalid_ids(rng)
             all_invalid_ids.extend(new_invalid_ids)
-        print(all_invalid_ids)
         print(sum(all_invalid_ids))
