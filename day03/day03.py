fe6380a3445d49b5a134c64fbcd27aefa6cc0bf1
Ben Kling <kling.ben@gmail.com>
2025-12-08 15:04:07 -0500

    [day03] solved part 1

--- day03/day03.py
+++ day03/day03.py
@@ -2,9 +2,22 @@ import typer
 
 app = typer.Typer()
 
-def get_max_joltage(bank:int):
+def get_max_joltage(bank:str):
     # print(f"getting max for {bank}")
-    return 0
+    max_joltage = 0
+
+    # for each element up to the second to last
+    # get the max remaining element and compare to current max jolt
+    # replace if higher
+    for i in range(0, len(bank) - 1):
+        bat1 = int(bank[i])
+        for j in range(i+1, len(bank)):
+            bat2 = int(bank[j])
+            joltage = int(f"{bat1}{bat2}")
+            if joltage > max_joltage:
+                max_joltage = joltage
+
+    return max_joltage
 
 
 @app.command()
@@ -12,11 +25,15 @@ def get(file_name:str):
     print(file_name)
     with open(file_name) as banks:
         idx = 0
+        jolt_sum = 0
         for bank in banks:
             bank = bank.strip()
             max_j = get_max_joltage(bank)
-            print(f"Bank #: {idx:5} = '{bank}'")
+            print(f"Bank #: {idx:5} = '{bank}' -> max joltage {max_j}")
             idx += 1
+            jolt_sum += max_j
+
+        print(jolt_sum)
     return
 
 
