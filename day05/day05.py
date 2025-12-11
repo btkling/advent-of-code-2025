08196f06f0b2d9b387eefdbddc44b66eb85bf421
Ben Kling <kling.ben@gmail.com>
2025-12-11 16:19:38 -0500

    [day05] complete

--- day05/day05.py
+++ day05/day05.py
@@ -4,7 +4,6 @@ app = typer.Typer()
 
 
 def parse_file(filename):
-
     fresh_ranges = []
     available_ids = []
     completed_fresh = False
@@ -42,44 +41,52 @@ def get_range_bounds(rng: str):
     return int(r_start), int(r_end)
 
 
+def do_ranges_intersect(s1, e1, s2, e2):
+
+    # 1-3 intersects 2-4
+    if (s1 <= s2) & (e1 >= s2):
+        return True
+    # 2-4 intersects 1-3
+    if (s1 >= s2) & (s1 <= e2):
+        return True
+    # 1-4 intersects 2-3
+    if (s1 <= s2) & (e1 >= e2):
+        return True
+    # 2-3 intersects 1-4
+    if (s1 >= s2) & (e1 <= e2):
+        return True
+
+    return False
+
+
+
+
+
 def count_fresh_amount(fresh_ranges:list[str]):
     ranges_list = []
-    overlapping_ranges = []
     for r in fresh_ranges:
+        overlapping_ranges = []
         r_start, r_end = get_range_bounds(r)
+        print(f"checking {r_start}-{r_end}")
         # print(f"checking {r_start=}, {r_end=} against {ranges_list}")
         if len(ranges_list) == 0:
             ranges_list.append([r_start, r_end])
         else:
             for rng_to_check in ranges_list:
-                # check_range_start = rng_to_check[0]
-                # check_range_end = rng_to_check[1]
-
-                # print(f"  -> eval for range {check_range_start} to {check_range_end}")
-
-                # ex 1: 4-6 overlaps with 3-5 because 4 is between 3 and 5
-                if (r_start >= rng_to_check[0]) & (r_start <= rng_to_check[1]):
-                    # print(f"   ! {r} overlaps with {rng_to_check}")
-                    # if the range starts somewhere after the start of another range, and before the end of another
-
-                    # in this instance we want to create a new range of 3-6 so pull the start up
-                    r_start = rng_to_check[0]
-                    # print(f"    -> new range = {r_start} - {r_end}")
-
-                    # mark the original range as overlapping
-                    overlapping_ranges.append(rng_to_check)
-
-                # ex 2: 4-6 overlaps with 5-9 because 6 is between 5 and 9
-                if (r_end >= rng_to_check[0]) & (r_end <= rng_to_check[1]):
-                    # print(f"   ! {r} overlaps with {rng_to_check}")
-                    # in this instance we want to create a new range of 4-9 so push the end out
-                    r_end = rng_to_check[1]
-
-                    # print(f"    -> new range = {r_start} - {r_end}")
-                    # mark the original range as overlapping
-                    overlapping_ranges.append(rng_to_check)
+                s1 = r_start
+                e1 = r_end
+                s2 = rng_to_check[0]
+                e2 = rng_to_check[1]
+                if do_ranges_intersect(s1, e1, s2, e2):
+                    print(f"  -> {s1}-{e1} overlaps with         {s2}-{e2}")
+                    r_start = min(s1, s2)
+                    r_end = max(e1, e2)
+                    overlapping_ranges.append([s2,e2])
+                else:
+                    print(f"  -> {s1}-{e1} does not overlap with {s2}-{e2}")
 
             # print(f"overlapping_ranges are {overlapping_ranges}")
+            print(f"  -> removing {overlapping_ranges}")
             ranges_list = [r for r in ranges_list if r not in overlapping_ranges]
             ranges_list.append([r_start, r_end])
 
